6755ac668daad70c513710592dad29778022d586
luziana13 <luziana1310@gmail.com>
2021-04-24 21:24:44 -0300

    lista de produto

--- lib/ui/home_adm.dart
+++ lib/ui/home_adm.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:e_commerce/helpers/products_class.dart';
 import 'package:e_commerce/ui/cadastrar_Product.dart';
 import 'package:flutter/material.dart';
@@ -12,15 +13,25 @@ class HomeAdm extends StatefulWidget {
 }
 
 class _HomeAdmState extends State<HomeAdm> {
-  var listProducts = new Product();
-  void _listProduct() async {
+  List<Product> list = [];
+  Future<List<Product>> listProducts() async {
     var _token = widget.user.token;
     Response response;
     Dio dio = new Dio();
     response = await dio.get(
         'https://restful-ecommerce-ufma.herokuapp.com/api/v1/products',
         options: Options(headers: {"Authorization": _token}));
-    print(response.data);
+    if (response.data["success"] != true) {
+      throw Exception("erro na requisição");
+    }
+
+    List<dynamic> lista = List<dynamic>.from(response.data["data"]);
+    list.clear();
+    for (var i in lista) {
+      list.add(Product.fromJson(i));
+    }
+    print(list.length);
+    return list;
   }
 
   void registerProduct(BuildContext context) async {
@@ -28,8 +39,10 @@ class _HomeAdmState extends State<HomeAdm> {
         MaterialPageRoute(builder: (context) => RegisterProduct(widget.user)));
   }
 
+  void deleteProd(BuildContext context, Product prod) {}
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
         appBar: AppBar(
             backgroundColor: Colors.red,
@@ -42,35 +55,108 @@ class _HomeAdmState extends State<HomeAdm> {
                 color: Colors.white,
               ),
             )),
-        body: //Colocar a lista aqui
-            TextButton(
-          onPressed: () {
-            registerProduct(context);
-          },
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        body: Column(children: [
+          Divider(),
+          TextButton(
+            onPressed: () {
+              registerProduct(context);
+            },
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Cadastrar produtos',
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
             ),
           ),
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.account_circle,
-                  color: Colors.white,
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'Cadastrar produtos',
-                  style: TextStyle(color: Colors.white),
-                )
-              ],
-            ),
-          ),
-        ));
+          Divider(),
+          FutureBuilder(
+              future: listProducts(),
+              builder: (context, AsyncSnapshot<List<Product>> snapshot) {
+                if (snapshot.hasError) {
+                  print(snapshot.hasError);
+                  return Text("Erro");
+                }
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Column(
+                    children: snapshot.data
+                        .map(
+                          (e) => Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border:
+                                    Border.all(color: Colors.grey, width: 2),
+                              ),
+                              width: width,
+                              height: 150,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(e.title,
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        )),
+                                  ),
+                                  Container(
+                                    alignment: Alignment.topRight,
+                                    child: Text(e.price.toString(),
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        )),
+                                  ),
+                                  Container(
+                                    alignment: Alignment.bottomLeft,
+                                    child: Text(e.description,
+                                        style: TextStyle(
+                                          fontSize: 10,
+                                          fontWeight: FontWeight.normal,
+                                        )),
+                                  ),
+                                  Container(
+                                      alignment: Alignment.bottomRight,
+                                      child: FloatingActionButton(
+                                        onPressed: () {
+                                          deleteProd(context, e);
+                                        },
+                                        child: const Icon(Icons.delete,
+                                            color: Colors.red),
+                                        backgroundColor: Colors.white,
+                                        mini: true,
+                                      )),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }),
+        ]));
   }
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:e_commerce/helpers/products_class.dart';
-import 'package:e_commerce/ui/cadastrar_Product.dart';
 import 'package:flutter/material.dart';
 import '../helpers/user_class.dart';
 import 'package:dio/dio.dart';
@@ -12,24 +11,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var listProducts = new Product();
-  void _listProduct() async {
+  List<Product> list = [];
+  Future<List<Product>> listProducts() async {
     var _token = widget.user.token;
     Response response;
     Dio dio = new Dio();
     response = await dio.get(
         'https://restful-ecommerce-ufma.herokuapp.com/api/v1/products',
         options: Options(headers: {"Authorization": _token}));
-    print(response.data);
+    if (response.data["success"] != true) {
+      throw Exception("erro na requisição");
+    }
+
+    List<dynamic> lista = List<dynamic>.from(response.data["data"]);
+    list.clear();
+    for (var i in lista) {
+      list.add(Product.fromJson(i));
+    }
+    print(list.length);
+    return list;
   }
 
-  void registerProduct(BuildContext context) async {
-    await Navigator.push(context,
-        MaterialPageRoute(builder: (context) => RegisterProduct(widget.user)));
-  }
-
+  void adicionarCarrinho(BuildContext context, Product prod) {}
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.red,
@@ -42,7 +48,74 @@ class _HomePageState extends State<HomePage> {
               color: Colors.white,
             ),
           )),
-      //future list
+      body: FutureBuilder(
+          future: listProducts(),
+          builder: (context, AsyncSnapshot<List<Product>> snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.hasError);
+              return Text("Erro");
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                children: snapshot.data
+                    .map(
+                      (e) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey, width: 2),
+                          ),
+                          width: width,
+                          height: 150,
+                          child: Column(
+                            children: [
+                              Container(
+                                alignment: Alignment.topLeft,
+                                child: Text(e.title,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                              Container(
+                                alignment: Alignment.topRight,
+                                child: Text(e.price.toString(),
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                              Container(
+                                alignment: Alignment.bottomLeft,
+                                child: Text(e.description,
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.normal,
+                                    )),
+                              ),
+                              Container(
+                                  alignment: Alignment.bottomRight,
+                                  child: FloatingActionButton(
+                                    onPressed: () {
+                                      adicionarCarrinho(context, e);
+                                    },
+                                    child: const Icon(Icons.add_shopping_cart),
+                                    mini: true,
+                                    backgroundColor: Colors.green,
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              );
+            }
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
     );
   }
 }
